b555c9eb097e58e703dac1a6b98d9d717c147bc6
D1esel3504 <davidovich94@mail.ru>
2021-09-03 22:57:16 +0300

    start diplom

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route>
+          Начало проекта
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
