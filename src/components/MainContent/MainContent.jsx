72e6735e311d75ef24fce2d7635afa00055176ba
Egor <davidovich94@mail.ru>
2021-09-09 15:09:53 +0300

    classnames

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styles from './MainContent.module.scss';
-import roomImage from '../../image/room.svg'
+import roomImage from '../../image/room.svg';
+import AddToPhotosOutlinedIcon from '@material-ui/icons/AddToPhotosOutlined';
+import classnames from 'classnames';
 
 const MainContent = () => {
   // const [rooms, setRooms] = useState([
@@ -12,56 +14,40 @@ const MainContent = () => {
 
   // })
 
-
   return (
-    <ul className={styles.main}>
-      <li className={styles.room}>
-        <span className={styles.room__title}>Conference Room #1</span>
-        <div className={styles.room__info}>
-          <img src={roomImage} alt="room" />
-          <div className={styles.room__description}>
-            <span>Guests: 17</span>
-            <span>Extra features:</span>
-            <span>Projector, Web Camera, Catering</span>
+    <div className={styles.main}>
+      <button className={styles.button__add}>
+        <AddToPhotosOutlinedIcon
+          style={{
+            fontSize: '4rem',
+          }}
+        />
+        <span className={styles.button__add__text}>ADD NEW ROOM</span>
+      </button>
+      <ul className={styles.rooms}>
+        <li className={styles.room}>
+          <div className={styles.info__block}>
+            <div className={styles.room__info}>
+              <span className={styles.room__title}>Conference Room #1</span>
+              <img className={styles.room__image} src={roomImage} alt="room" />
+            </div>
+            <div className={styles.room__description}>
+              <span>Guests: 12</span>
+              <span>Extra features:</span>
+              <span>Projector, Web Camera, Catering</span>
+            </div>
           </div>
-          <div className={styles.room__buttons}>
-            <button className={styles.room__buttons__more}>Watch more</button>
-            <button className={styles.room__buttons__delete}>Delete room</button>
+          <div>
+            <button className={classnames(styles.room__buttons__more, styles.room__buttons)}>
+              Watch more
+            </button>
+            <button className={classnames(styles.room__buttons__delete, styles.room__buttons)}>
+              Delete room
+            </button>
           </div>
-        </div>
-      </li>
-      <li className={styles.room}>
-        <span className={styles.room__title}>Conference Room #1</span>
-        <div className={styles.room__info}>
-          <img src={roomImage} alt="room" />
-          <div className={styles.room__description}>
-            <span>Guests: 17</span>
-            <span>Extra features:</span>
-            <span>Projector, Web Camera, Catering</span>
-          </div>
-          <div className={styles.room__buttons}>
-            <button className={styles.room__buttons__more}>Watch more</button>
-            <button className={styles.room__buttons__delete}>Delete room</button>
-          </div>
-        </div>
-      </li>
-      <li className={styles.room}>
-        <span className={styles.room__title}>Conference Room #1</span>
-        <div className={styles.room__info}>
-          <img src={roomImage} alt="room" />
-          <div className={styles.room__description}>
-            <span>Guests: 17</span>
-            <span>Extra features:</span>
-            <span>Projector, Web Camera, Catering</span>
-          </div>
-          <div className={styles.room__buttons}>
-            <button className={styles.room__buttons__more}>Watch more</button>
-            <button className={styles.room__buttons__delete}>Delete room</button>
-          </div>
-        </div>
-      </li>
-
-    </ul>
+        </li>
+      </ul>
+    </div>
   );
 };
 
